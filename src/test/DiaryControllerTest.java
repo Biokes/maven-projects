8e3300cdf699c27285955810fba915abe5bcca9e
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-23 17:58:22 +0100

    commit

--- src/test/DiaryControllerTest.java
+++ src/test/DiaryControllerTest.java
@@ -1,2 +1,11 @@
 public class DiaryControllerTest{
+    private void print(String word){
+        System.out.println(word);
+    }
+    public void mainMenu(){
+        print("""
+Welcome to DDiary
+
+""");
+    }
 }
