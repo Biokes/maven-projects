e2594605e86d80c759bb697709272fef277e4432
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-23 18:17:59 +0100

    commit message
    Signed-off-by: Biokes <106444765+Biokes@users.noreply.github.com>

--- src/test/DiaryControllerTest.java
+++ src/test/DiaryControllerTest.java
@@ -1,11 +1,49 @@
+import java.util.Scanner;
+
 public class DiaryControllerTest{
+    private final Scanner scanner=new Scanner(System.in);
+    private DiaryServices diaryServices=new DiaryServiceImpo( );
     private void print(String word){
         System.out.println(word);
     }
-    public void mainMenu(){
-        print("""
-Welcome to DDiary
 
+    private String input(String word){
+        print(word);
+        return scanner.next( );
+    }
+
+    public String mainMenu(){
+        return input("""
+                Welcome to D-Diary
+                1. Sign up
+                2. log in
+                3. Exit
 """);
+    }
+
+    public void signUp(){
+        try{
+            String username=input("Enter your preferred name: ");
+            String password=input("Enter your preferred password : ");
+
+        }catch( RuntimeException error ){
+            System.out.println(error.getMessage( ));
+            mainMenu( );
+        }
+        System.out.println("Diary Successfully Created.");
+        diaryMenu( );
+    }
+
+    public void menu(String choice){
+        switch(choice){
+            case "1" -> signUp( );
+            case "2" -> logIn( );
+            case "3" -> exit( );
+            default -> mainMenu( );
+        }
+    }
+
+    public void diaryMenu(){
+
     }
 }
