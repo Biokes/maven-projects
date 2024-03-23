27115035d5cc98ed82aa59391954cc571b48bfe6
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-23 18:29:08 +0100

    commit

--- src/test/DiaryControllerTest.java
+++ src/test/DiaryControllerTest.java
@@ -4,6 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 public class DiaryControllerTest{
     private DiaryController controller = new DiaryController();
     @BeforeEach public void initialize(){
-
+        controller = new DiaryController();
     }
 }
