23f5537c4539597103936576ff227d221330ad9c
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-23 18:32:42 +0100

    commit

--- src/test/DiaryControllerTest.java
+++ src/test/DiaryControllerTest.java
@@ -3,7 +3,15 @@ import org.junit.jupiter.api.BeforeEach;
 
 public class DiaryControllerTest{
     private DiaryController controller = new DiaryController();
-    @BeforeEach public void initialize(){
+    @BeforeEach
+    public void initializer(){
         controller = new DiaryController();
     }
+    @Test
+    public void testCreateDiary_testDiaryIsCReatedProperly(){
+
+    }
+    @Test public void testLogInAndLogOutProperly(){}
+    @Test public void testCreateEntryAndDeleteEntryProperly(){}
+    @Test public void testUpdateEntryProperly(){}
 }
