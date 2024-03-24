af44c557a000d8f620827d3cd4647da2a2e32d50
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-24 16:39:34 +0100

    commit

--- src/diaryPackages/dtos/LoginRequest.java
+++ src/diaryPackages/dtos/LoginRequest.java
@@ -1,30 +1,9 @@
 package dtos;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest{
     private String password;
     private String userName;
-
-    public LoginRequest(){
-    }
-
-    public LoginRequest(String userName, String password){
-        this.userName=userName;
-        this.password=password;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
-        this.password=password;
-    }
-
-    public String getUserName(){
-        return userName;
-    }
-
-    private void setUserName(String userName){
-        this.userName=userName;
-    }
 }
