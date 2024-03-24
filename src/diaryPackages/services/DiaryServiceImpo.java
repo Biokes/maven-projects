1c95cd73bd59caf9c1e7f24a5359d329346e6ae7
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-24 15:30:51 +0100

    commit

--- src/diaryPackages/services/DiaryServiceImpo.java
+++ src/diaryPackages/services/DiaryServiceImpo.java
@@ -8,6 +8,7 @@ import data.repositories.EntryRepo;
 import data.repositories.EntryRepoImpo;
 import dtos.LoginRequest;
 import dtos.RegisterDiary;
+import dtos.dtos.EntryRequest;
 import exceptions.DiaryNotFoundException;
 import exceptions.IncorrectPasswordException;
 import exceptions.InvalidDetailsException;
