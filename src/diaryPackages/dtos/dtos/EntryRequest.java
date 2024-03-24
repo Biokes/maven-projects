d1f2709feb9b939ba9d265f216fd67b7df0aa1ab
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-24 16:41:19 +0100

    commit

--- src/diaryPackages/dtos/dtos/EntryRequest.java
+++ src/diaryPackages/dtos/dtos/EntryRequest.java
@@ -1,44 +1,11 @@
 package dtos.dtos;
 
+import lombok.Data;
+
+@Data
 public class EntryRequest{
     private String userName;
     private String title;
     private String body;
     private int id;
-    public EntryRequest(){
-
-    }
-
-    public EntryRequest(String title, String body){
-        setTitle(title);
-        setbody(body);
-    }
-
-    public String getUserName(){
-        return userName;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public void setTitle(String title){
-        this.title=title;
-    }
-
-    public String getBody(){
-        return body;
-    }
-
-    public int getId(){
-        return id;
-    }
-
-    public void setUserName(String userName){
-        this.userName=userName;
-    }
-
-    public void setbody(String body){
-        this.body=body;
-    }
 }
