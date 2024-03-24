b111e9e88ec9bc126d9eda171bcd73d60461d375
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-24 15:28:29 +0100

    commit

--- src/diaryPackages/data/moodels/Diary.java
+++ src/diaryPackages/data/moodels/Diary.java
@@ -5,10 +5,8 @@ import lombok.Data;
 import java.lang.annotation.Documented;
 
 @Data
-@Document
 public class Diary{
     private boolean isLocked=false;
-    @id
     private String username;
     private String password;
 
