8c2037d61a29ccbc18d88b2ce5a252d7eddee95a
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-25 18:40:32 +0100

    commit

--- src/diaryPackages/services/EntryServicesImpo.java
+++ src/diaryPackages/services/EntryServicesImpo.java
@@ -1,4 +1,16 @@
 package services;
 
+import data.repositories.EntryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EntryServicesImpo implements EntryServices{
+    @Autowired
+    private EntryRepo entryRepo;
+    public int countEntries(){
+        return (int)entryRepo.count( );
+    }
+
+
 }
